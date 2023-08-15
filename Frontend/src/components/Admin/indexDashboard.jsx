e5c3f440c7a42ffb7cec17ce00346c78efc0c771
Sergio Galvis <chefosergio@gmail.com>
2023-08-14 20:12:17 -0500

    iniciando diseño

--- Frontend/src/components/Admin/indexDashboard.jsx
+++ Frontend/src/components/Admin/indexDashboard.jsx
@@ -1,9 +1,38 @@
-import {} from "./styledDashboard"
+import { LinkContainer, LinkIcon, LinkMenu, Logo, LogoName, NavLink, Sidebar, SidebarMenu } from "./styledDashboard"
+import {RxDashboard} from "react-icons/rx"
+
+
 
 const Dashboard = () => {
+  const CustomLink = ({icon, label, to, id}) => {
+    return(
+      <LinkContainer key={id}>
+        <NavLink to={to}>
+          <LinkIcon>{icon}</LinkIcon>
+          <span>{label}</span>
+        </NavLink>  
+      </LinkContainer>
+    )
+  }
   return ( 
     <div>
-      <h1>dashboard</h1>
+      {/* Sidebar */}
+    <Sidebar>
+      <Logo to="/" >
+        <RxDashboard/>
+        <LogoName><span>Cars</span>Transformations</LogoName>
+      </Logo>
+      <SidebarMenu>
+        {LinkMenu.map(({icon, label, to, id}) =>(
+          <CustomLink
+            key={id}
+            icon={icon}
+            label={label}
+            to={to}
+          />
+        ))}
+      </SidebarMenu>
+    </Sidebar>
     </div>
   );
 }
--- Frontend/src/components/Admin/styledDashboard.jsx
+++ Frontend/src/components/Admin/styledDashboard.jsx
@@ -1,5 +1,81 @@
 import styled from 'styled-components';
+import {Link} from "react-router-dom"
 
-export const ContainerDashboard = styled.div`
+export const ContainerDashboard = styled.body`
   
 `;
+
+/* SideBar Init */
+export const Sidebar = styled.div`
+  
+`;
+
+export const Logo = styled.a`
+  
+`;
+
+export const LogoName = styled.div`
+  
+
+`;
+
+export const SidebarMenu = styled.ul`
+  
+`;
+
+export const LinkContainer = styled.div`
+  
+`;
+
+export const LinkIcon = styled.div`
+  
+`;
+
+export const NavLink = styled(Link)`
+  
+`; 
+
+export const LinkMenu = [
+  {
+    id:"1",
+    label:"Home",
+    icon: "",
+    to:"/"
+  },
+  {
+    id:"2",
+    label:"Productos",
+    icon: "",
+    to:"/admin/productos"
+  },
+  {
+    id:"3",
+    label:"Clientes",
+    icon: "",
+    to:"/admin/clientes"
+  },
+  {
+    id:"4",
+    label:"Vehiculos",
+    icon: "",
+    to:"/admin/vehiculos"
+  },
+  {
+    id:"5",
+    label:"Home",
+    icon: "",
+    to:"/admin/productos"
+  },
+  {
+    id:"6",
+    label:"Home",
+    icon: "",
+    to:"/admin/productos"
+  },
+  {
+    id:"7",
+    label:"Home",
+    icon: "",
+    to:"/admin/productos"
+  }
+]
