ad82e96c930d8f75e1107d5e0cb2e84de7cc266e
Sergio Galvis <Sgalvis@ulearning.com.co>
2023-08-18 16:43:09 -0500

    se actualizo la vista cliente v1

--- Frontend/src/components/Client/Navbar.jsx
+++ Frontend/src/components/Client/Navbar.jsx
@@ -4,9 +4,12 @@ import {
   NavLogo,
   NavMenuUl,
   NavbarContent,
+  NavMenuLi
 } from "./styles/StyledNavbar";
+import {  MenuOptions} from "./archive/MenuOptions"
 import { useState } from "react";
 import logo from '../../assets/slider/LOGO-PAGE.png'
+import {Link} from "react-router-dom"
 
 function Navbar() {
   const [click, setClick] = useState();
@@ -22,7 +25,15 @@ function Navbar() {
           <i className="fas fa-bars"></i>
         </MenuIcons>
         <NavMenuUl click={click}>
-
+          {MenuOptions.map((item, i) => {
+              return (
+                <NavMenuLi key={i}>
+                  <Link className={item.class} to={item.path}>
+                    <i className={item.icon}></i>{item.title}
+                  </Link>
+                </NavMenuLi>
+              );
+            })}
         </NavMenuUl>
         {MenuOptions.find((item) => item.id === 7) && (
           <NavMenuLi style={{listStyle:"none"}}>
