7e6beaf4cf8232f9f88cb8104e6bf37e1a938279
johandev99 <jdiaz@ulearning.com.co>
2023-08-19 12:29:57 -0500

    fix footer

--- Frontend/src/components/Client/Footer.jsx
+++ Frontend/src/components/Client/Footer.jsx
@@ -1,11 +1,34 @@
-import { Fotcontain } from "./styles/StyledFooter";
+import { About, Firstbox, Footh5, Footli, Footul, Fotcontain, FtAnchor, Help, Logo, Reserved, Stayinput } from "./styles/StyledFooter";
 
 
 function Footer() {
   return (
     <>
     <Fotcontain>
-      
+      <Firstbox>
+        <Logo src="" alt="image"/>
+        <Help>
+          <Footh5>Ayuda</Footh5>
+          <Footul>
+            <Footli>
+              <FtAnchor href="/">PQR</FtAnchor>
+              <FtAnchor href="/">Atencion</FtAnchor>
+              <FtAnchor href="/">Contacta</FtAnchor>
+            </Footli>
+          </Footul>
+        </Help>
+        <About>
+          <Footh5>Acerca de nosotros</Footh5>
+          <FtAnchor href="/">About Spec</FtAnchor>
+          <FtAnchor href="/">Careers</FtAnchor>
+        </About>
+        <Stayinput>
+          <Footh5>Stay up to date</Footh5>
+        </Stayinput>
+      </Firstbox>
+      <Reserved>
+        <></>
+      </Reserved>
     </Fotcontain>
     </>
   )
--- Frontend/src/components/Client/styles/StyledFooter.jsx
+++ Frontend/src/components/Client/styles/StyledFooter.jsx
@@ -5,3 +5,56 @@ export const Fotcontain = styled.div`
   width: 100%;
   height: 35vh;
 `;
+
+export const Firstbox = styled.div`
+  background-color: #eee;
+  display: flex;
+  width: 100%;
+  height: 30vh;
+`;
+
+export const Logo = styled.img`
+  background-color: red;
+  width: 25%;
+  height: 30vh;
+`;
+
+export const Help = styled.div`
+  background-color: red;
+  width: 25%;
+  height: 30vh;
+`;
+
+export const Footh5 = styled.h5`
+  
+`;
+
+export const Footul = styled.ul`
+  
+`;
+
+export const Footli = styled.li`
+  
+`;
+
+export const FtAnchor = styled.a`
+  
+`;
+
+export const About = styled.div`
+  background-color: red;
+  width: 25%;
+  height: 30vh;
+`;
+
+export const Stayinput = styled.div`
+  background-color: red;
+  width: 25%;
+  height: 30vh;
+`;
+
+export const Reserved = styled.div`
+  background-color: #f5f;
+  width: 100%;
+
+`;
