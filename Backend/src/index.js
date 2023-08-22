dc28e4f3800570b3c28ad97579f92ecb42253dca
FundacionET <fundacionet4@gmail.com>
2023-08-22 15:56:02 -0500

    todo correcto

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -1,10 +1,9 @@
 import express from "express";
 // import bodyParser from "body-parser";
-import router from "./routes/users.route.js";
+import router from "./routes/customer.route.js";
+import router1 from "./routes/employed.route.js";
+import router2 from "./routes/inventory.route.js";
 import cors from "cors"
-
-
-
 
 const app = express();
 
@@ -13,7 +12,7 @@ app.use(express.json());
 app.use(cors());
 //app.use(bodyParser.json());
 //final backend
-app.use(router)
+app.use(router, router1, router2)
 
 app.listen(3005, () =>{
   console.log(`estoy corriendo en el puerto 3005`)
