b5aaf10fcadb7c9f47bc520fed71e1d9fcdbb11d
Sergio Galvis <Sgalvis@ulearning.com.co>
2023-08-23 13:52:39 -0500

    creando consultas vista admin v5.1.0

--- Frontend/src/components/Admin/Pages/Home.jsx
+++ Frontend/src/components/Admin/Pages/Home.jsx
@@ -1,56 +1,95 @@
 import { useEffect, useState } from "react";
-import { ContainerHeader, ContainerHome, CustomerCards, Header} from "./Styles/styledHome";
-import axios from "axios"
+import {
+  ContainerHeader,
+  ContainerHome,
+  Cards,
+  Header,
+  CardsContainer,
+  Cards2,
+  ContainerCards,
+} from "./Styles/styledHome";
+/* import axios from "axios" */
+
 
 const Home = () => {
   const Reloj = () => {
     const [time, setTime] = useState(new Date());
-  
+
     useEffect(() => {
       const interval = setInterval(() => {
         setTime(new Date());
       }, 1000);
-  
+
       return () => clearInterval(interval);
     }, []);
-  
-    let mensaje = '';
+
+    let mensaje = "";
 
     const hora = time.getHours();
     if (hora < 12) {
-      mensaje = 'Good Morning, Sergio Galvis';
-    } else if (hora < "18:00:00 ") {
-      mensaje = 'Good , Sergio Galvis';
+      mensaje = "Good Morning, Sergio Galvis";
+    } else if (hora < 15) {
+      mensaje = "Good Afternoon, Sergio Galvis";
     } else {
-      mensaje = 'Good Evening, Sergio Galvis';
+      mensaje = "Good Evening, Sergio Galvis";
     }
-  
+
     return (
-        <Header><h1>{mensaje}</h1><h1>{time.toLocaleTimeString()}</h1></Header>
+      <Header>
+        <h1>{mensaje}</h1>
+        <h1>{time.toLocaleTimeString()}</h1>
+      </Header>
     );
   };
 
-
+/*  const AccountantQueries = () => {
+    useEffect(() => {
+      const queries = [
+        '/api/consultas1',
+        '/api/consultas2',
+        '/api/consultas3',
+      ];
   
-  const [count, setCount] = useState("")
-
-  const Customers = () => {
-    const res = axios.get("http://localhost:3005/customer/count")
-    setCount(res.data)
-  }
-
-  return ( 
-      <ContainerHome>
-        <ContainerHeader>
-          <Reloj/>
-        </ContainerHeader>
-        <CustomerCards>
-            {Customers}
-            {count}
-        </CustomerCards>
-      </ContainerHome>
-
+      queries.forEach((consulta) => {
+        axios.get(consulta)
+          .then(res => {
+            console.log(res.data);
+          })
+          .catch(err => {
+            console.error("Error al obtener datos", err);
+          });
+      });
+    }, []);
+  
+    return null; 
+  }; */
+  
+  return (
+    <ContainerHome>
+      <ContainerHeader>
+        <Reloj />
+      </ContainerHeader>
+      <ContainerCards>
+        <CardsContainer>
+          <Cards></Cards>
+          <Cards></Cards>
+          <Cards></Cards>
+          <Cards></Cards>
+        </CardsContainer>
+        <CardsContainer>
+          <Cards></Cards>
+          <Cards></Cards>
+          <Cards></Cards>
+          <Cards></Cards>
+        </CardsContainer>
+        <CardsContainer>
+          <Cards2></Cards2>
+          <Cards2 style={{ width: "750px" }}></Cards2>
+          <Cards2></Cards2>
+        </CardsContainer>
+      </ContainerCards>
+    </ContainerHome>
   );
-}
+};
 
 export default Home;
