e14a7dfc8d3b137ae53a06b69a863121a9afbcf0
Sergio Galvis <chefosergio@gmail.com>
2023-08-27 17:49:05 -0500

    actualizacion vista adminv9

--- Frontend/src/components/Admin/Pages/Styles/styledHome.jsx
+++ Frontend/src/components/Admin/Pages/Styles/styledHome.jsx
@@ -3,6 +3,10 @@ import styled from 'styled-components';
 export const ContainerHome = styled.div`
   width: 97%;
   height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
 `;
 export const ContainerHeader = styled.div`
   width: 100%;
@@ -30,16 +34,15 @@ export const ContainerCards = styled.div`
   justify-content: space-around;
 `;
 export const CardsContainer = styled.div`
-  height:auto;
+  height: 30%;
   width: 100%;
   display: flex;
   justify-content: space-evenly;
   align-items: center;
-  background-color:red;
 `;
 export const Cards = styled.div`
-  width: 370px;
-  height: 220px;
+  width: 23%;
+  height: 100%;
   border-radius: 15px;
   color: black;
   background-color: white;
