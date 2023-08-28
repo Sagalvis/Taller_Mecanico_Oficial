1f52cc1e6249b417a797935194fdaedc76cd7c38
johandev99 <jdiaz@ulearning.com.co>
2023-08-28 17:04:35 -0500

    se realiza registro de vehiculos

--- Frontend/src/components/Client/VehicleRegister.jsx
+++ Frontend/src/components/Client/VehicleRegister.jsx
@@ -1,18 +1,72 @@
 import Navbar from "./Navbar";
-import { ContainRegisters, ContainerVReg } from "./styles/StyledVehicleRegister";
-
+import {
+  ContainInfo,
+  ContainRegisters,
+  ContainerVReg,
+  InfoStatus,
+  InfoText,
+  InfoVehicle,
+  Registers,
+  Statustext,
+  Title,
+} from "./styles/StyledVehicleRegister";
 
 function VehicleRegister() {
   return (
     <>
-    <Navbar />
-    <ContainerVReg>
-      <ContainRegisters>
-        <h1>Registros del vehículo</h1>
-      </ContainRegisters>
-    </ContainerVReg>
+      <Navbar />
+      <ContainerVReg>
+        <ContainRegisters>
+          <h2>Registros del vehículo</h2>
+          <Registers>
+            <InfoStatus>
+              <Statustext>Estado: En proceso</Statustext>
+              <Statustext>Entrada: 28/08/2023</Statustext>
+              <Statustext>Salida: 00/00/0000</Statustext>
+            </InfoStatus>
+
+            <InfoVehicle>
+              <div className="info">
+            <ContainInfo>
+              <Title>Nombre(s): </Title>
+              <InfoText>JOHAN DAVID DÍAZ QUINTERO</InfoText>
+            </ContainInfo>
+            <ContainInfo>
+              <Title>Documento: </Title>
+              <InfoText>1143168571</InfoText>
+            </ContainInfo>
+            <ContainInfo>
+              <Title>Placa: </Title>
+              <InfoText>AEP41G</InfoText>
+            </ContainInfo>
+            <ContainInfo>
+              <Title>Marca: </Title>
+              <InfoText>BOXER</InfoText>
+            </ContainInfo>
+            <ContainInfo>
+              <Title>Modelo: </Title>
+              <InfoText>CT 102 KS 2022</InfoText>
+            </ContainInfo>
+            </div>
+            
+            <ContainInfo>
+              <Title>Motivo de ingreso: </Title>
+              <InfoText>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Asperiores possimus ducimus eligendi eos molestiae vitae consequatur adipisci impedit corrupti expedita.</InfoText>
+            </ContainInfo>
+            <ContainInfo>
+              <Title>Info mecanico: </Title>
+              <InfoText>Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic, tenetur.</InfoText>
+            </ContainInfo>
+            <ContainInfo>
+              <Title>Mecanico responsable: </Title>
+              <InfoText>Sergio Galvis</InfoText>
+            </ContainInfo>
+            </InfoVehicle>
+          </Registers>
+        </ContainRegisters>
+      </ContainerVReg>
     </>
-  )
+  );
 }
 
 export default VehicleRegister;
--- Frontend/src/components/Client/styles/StyledVehicleRegister.jsx
+++ Frontend/src/components/Client/styles/StyledVehicleRegister.jsx
@@ -2,22 +2,76 @@ import styled from 'styled-components';
 
 export const ContainerVReg = styled.div`
   display: flex; 
-  background-color: green;
+  /* background-color: green; */
   flex-direction: column;
   width: 100%;
   height: 91vh;
   justify-content: center;
   align-items: center;
-  gap: 2rem;
 `;
 
 export const ContainRegisters = styled.div`
-  background-color: #eee;
-  text-align: center;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  background-color: #dcdcdc;
   padding: 1%;
   width: 50%;
   height: 80vh;
   border-radius: 4px;
   box-shadow: 9px 17px 22px -24px rgba(0,0,0,0.79);
   overflow-y: scroll;
+  overflow-x: hidden;
+`;
+
+export const Registers = styled.div`
+  width: 85%;
+  height: 30%; //18%
+  margin-top: 1.2rem;
+  padding: .7rem;
+  background-color: #fff;
+  border-radius: 5px;
+  overflow-y: scroll;
+  overflow-x: hidden;
+  box-shadow: 9px 17px 22px -24px rgba(0,0,0,0.79);
+`;
+
+export const InfoStatus = styled.div`
+  /* background-color: #fff; */
+  width: 100%;
+  height: 15%;
+  display: flex;
+  justify-content: flex-end;
+  gap: 12px;
+  font-weight: 500;
+  
+`;
+
+export const Statustext = styled.p`
+  margin: 0;
+`;
+
+export const InfoVehicle = styled.div`
+  display: flex;
+  flex-direction: column;
+  .info {
+    display: flex;
+    margin-bottom: 1rem;
+  }
+`;
+
+export const ContainInfo = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin-top: .5rem;
+  width: 100%;
+`;
+
+export const Title = styled.p`
+  font-weight: 500;
+  margin: 0 6.4rem 0 0;
+`;
+
+export const InfoText = styled.p`
+  margin: 0;
 `;
