d2a003906d764da09c1e25fa24054ccfe1f60fe3
Roy polo <roypolocarrillo@gmail.com>
2023-08-29 14:07:52 -0500

    se borro de index la ruta registerh

--- Frontend/src/components/index.jsx
+++ Frontend/src/components/index.jsx
@@ -8,26 +8,23 @@ import Register from './LoginRegister/Register/indexRegister';
 import ServiceAdvisor from "./ServiceAdvisor/indexAdvisor"
 import RegisterAdvisor from './ServiceAdvisor/Pages/Register';
 import RegisterVehicle from './ServiceAdvisor/Pages/registervehicle';
-import RegisterHojaV from './ServiceAdvisor/Pages/registerHoja';
-
 
 const Main = () => {
-  return ( 
+  return (
     <div>
-        <ContainerMain>
-            <Routes>
-              <Route path='*' element = {<HomeClient/>}/>
-              <Route path='/admin/*' element = {<Dashboard/>}/>
-              <Route path='/inventory/*' element = {<Inventory/>} />
-              <Route path='/advisor/*' element={<ServiceAdvisor/>}/>
-              <Route path='/employes/login' element = {<Login/>} />
-              <Route path='/employes/register' element = {<Register/>}/> 
-              <Route path='/registerc' element = {<RegisterAdvisor/>}/>
-              <Route path='/registerv' element = {<RegisterVehicle/>}/>
-              <Route path='/registerh' element = {<RegisterHojaV/>}/>
-            </Routes>
-          
-        </ContainerMain>
+      <ContainerMain>
+        <Routes>
+          <Route path='*' element={<HomeClient />} />
+          <Route path='/admin/*' element={<Dashboard />} />
+          <Route path='/inventory/*' element={<Inventory />} />
+          <Route path='/advisor/*' element={<ServiceAdvisor />} />
+          <Route path='/employes/login' element={<Login />} />
+          <Route path='/employes/register' element={<Register />} />
+          <Route path='/registerc' element={<RegisterAdvisor />} />
+          <Route path='/registerv' element={<RegisterVehicle />} />
+        </Routes>
+
+      </ContainerMain>
     </div>
   );
 }
