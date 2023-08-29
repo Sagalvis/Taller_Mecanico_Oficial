2dd7bd69a57f957685c68016b389a0e07b6cbfa2
Sergio Galvis <Sgalvis@ulearning.com.co>
2023-08-29 17:02:58 -0500

    vista registro Hoja

--- Frontend/src/components/ServiceAdvisor/Pages/registerHoja.jsx
+++ Frontend/src/components/ServiceAdvisor/Pages/registerHoja.jsx
@@ -1,6 +1,18 @@
 import { useState } from "react";
-import { ContainerRegister } from "./styles/styledRegister";
-import { ContainerHeaderRegisterH, ContainerRegisterH, TextArea, TitleRegisterH } from "./styles/styledRegisterH";
+import {
+  Button,
+  ContainH2,
+  ContainerRegister,
+  Form,
+  Input,
+  TitleH2,
+} from "./styles/styledRegister";
+import {
+  ContainerFormH,
+  ContainerRegisterH,
+  TextArea,
+  TitleRegisterH,
+} from "./styles/styledRegisterH";
 
 const RegisterHojaV = () => {
   const [cedula, setCedula] = useState("");
@@ -10,65 +22,81 @@ const RegisterHojaV = () => {
   const [motor, setMotor] = useState("");
   const [descripcionMecanico, setDescripcionMecanico] = useState("");
 
+  function acceptNum(evt) {
+    const input = evt.target.value;
+    evt.target.value = input.replace(/[^\d]/g, "");
+  }
   return (
     <ContainerRegister>
       <ContainerRegisterH>
-        <ContainerHeaderRegisterH>
+        <ContainerFormH>
+          <TitleRegisterH>
+            <h2>Formulario registro hoja de vida vehículos</h2>
+          </TitleRegisterH>
 
-        <TitleRegisterH>
-
-        <h1>Formulario registro hoja de vida</h1>
-        </TitleRegisterH>
-        </ContainerHeaderRegisterH>
-        <form>
-          <input
-            type="number"
-            placeholder="Cédula"
-            value={cedula}
-            onChange={(e) => setCedula(e.target.value)}
-          />
-          <input
-            type="text"
-            placeholder="Placa"
-            value={placa}
-            onChange={(e) => setPlaca(e.target.value)}
-          />
-          <TextArea
-            type="text"
-            rows={10}
-            cols={50}
-            placeholder="Estado ingreso vehículo"
-            value={estadoIngreso}
-            onChange={(e) => setEstadoIngreso(e.target.value)}
-          />
-          <input
-            type="number"
-            placeholder="Cilindraje"
-            value={cilindraje}
-            onChange={(e) => setCilindraje(e.target.value)}
-          />
-          <input
-            type="text"
-            placeholder="Motor"
-            value={motor}
-            onChange={(e) => setMotor(e.target.value)}
-          />
-          <TextArea
-            type="text"
-            rows={10}
-            cols={50}
-            placeholder="Descripción mecánico"
-            value={descripcionMecanico}
-            onChange={(e) => setDescripcionMecanico(e.target.value)}
-          />
-        </form>
-
-        <button type="submit">Enviar</button>
+          <Form>
+            <ContainH2>
+              <TitleH2>Datos del vehículo</TitleH2>
+            </ContainH2>
+            <Input
+              type="text"
+              placeholder="Cédula"
+              value={cedula}
+              onInput={(evt) => acceptNum(evt)}
+              maxLength={15}
+              onChange={(e) => setCedula(e.target.value)}
+            />
+            <Input
+              type="text"
+              placeholder="Placa"
+              value={placa}
+              onChange={(e) => setPlaca(e.target.value)}
+            />
+            <TextArea
+              type="text"
+              rows={10}
+              cols={50}
+              placeholder="Estado ingreso vehículo"
+              value={estadoIngreso}
+              onChange={(e) => setEstadoIngreso(e.target.value)}
+            />
+            <TextArea
+              type="text"
+              rows={3}
+              cols={50}
+              placeholder="Motivo ingreso vehículo"
+              value={estadoIngreso}
+              onChange={(e) => setEstadoIngreso(e.target.value)}
+            />
+            <Input
+              type="text"
+              onInput={(evt) => acceptNum(evt)}
+              maxLength={5}
+              placeholder="Cilindraje"
+              value={cilindraje}
+              onChange={(e) => setCilindraje(e.target.value)}
+              required
+            />
+            <Input
+              type="text"
+              placeholder="Motor"
+              value={motor}
+              onChange={(e) => setMotor(e.target.value)}
+            />
+            <TextArea
+              type="text"
+              rows={10}
+              cols={50}
+              placeholder="Descripción mecánico"
+              value={descripcionMecanico}
+              onChange={(e) => setDescripcionMecanico(e.target.value)}
+            />
+            <Button type="submit">Enviar</Button>
+          </Form>
+        </ContainerFormH>
       </ContainerRegisterH>
     </ContainerRegister>
   );
 };
 
 export default RegisterHojaV;
-
-
