fad858cefdcf1e0882c98c6ceb6ec41389183ca4
johandev99 <jdiaz@ulearning.com.co>
2023-08-29 15:36:35 -0500

    vista register

--- Frontend/src/components/ServiceAdvisor/Pages/Register.jsx
+++ Frontend/src/components/ServiceAdvisor/Pages/Register.jsx
@@ -1,9 +1,7 @@
 import { useState } from "react";
-import { ContainerRegister } from "./styles/styledRegister";
+import { ContainForm, ContainerRegister, Form, Input, Button, TitleH1 } from "./styles/styledRegister";
 
 const RegisterAdvisor = () => {
-
-  const FormularioRegistro = () => {
     const [nombre, setNombre] = useState("");
     const [apellidos, setApellidos] = useState("");
     const [correo, setCorreo] = useState("");
@@ -19,7 +17,7 @@ const RegisterAdvisor = () => {
         correo,
         numeroDocumento,
         direccion,
-        telefono
+        telefono,
       });
       // Limpia los campos del formulario
       setNombre("");
@@ -29,62 +27,62 @@ const RegisterAdvisor = () => {
       setDireccion("");
       setTelefono("");
     };
+  return (
+    <ContainerRegister>
 
-    return (
-      <form onSubmit={handleSubmit}>
-        <input
-          type="text"
-          placeholder="Nombre"
-          value={nombre}
-          onChange={(e) => setNombre(e.target.value)}
-          required
-        />
-        <input
-          type="text"
-          placeholder="Apellidos"
-          value={apellidos}
-          onChange={(e) => setApellidos(e.target.value)}
-          required
-        />
-        <input
-          type="email"
-          placeholder="Correo"
-          value={correo}
-          onChange={(e) => setCorreo(e.target.value)}
-          required
-        />
-        <input
-          type="text"
-          placeholder="Dirección"
-          value={direccion}
-          onChange={(e) => setDireccion(e.target.value)}
-          required
-        />
-          <input
+      <ContainForm>
+        <div style={{width: '40%', height: '30%', backgroundColor: '#000'}}>
+      <TitleH1>Formulario registro clientes</TitleH1>
+      </div>
+        <Form onSubmit={handleSubmit}>
+          <Input
+            type="text"
+            placeholder="Nombre"
+            value={nombre}
+            onChange={(e) => setNombre(e.target.value)}
+            required
+          />
+          <Input
+            type="text"
+            placeholder="Apellidos"
+            value={apellidos}
+            onChange={(e) => setApellidos(e.target.value)}
+            required
+          />
+          <Input
+            type="email"
+            placeholder="Correo"
+            value={correo}
+            onChange={(e) => setCorreo(e.target.value)}
+            required
+          />
+          <Input
+            type="text"
+            placeholder="Dirección"
+            value={direccion}
+            onChange={(e) => setDireccion(e.target.value)}
+            required
+          />
+          <Input
             type="number"
             placeholder="Número de documento"
             value={numeroDocumento}
             onChange={(e) => setNumeroDocumento(e.target.value)}
             required
           />
-          <input
+          <Input
             type="phone"
             placeholder="Telefono"
             value={telefono}
             onChange={(e) => setTelefono(e.target.value)}
             required
           />
-        
-        <button type="submit">Enviar</button>
-      </form>
-    );
-  };
-  return ( 
-    <ContainerRegister>
-      <h1>Formulario registro clientes</h1>
-      <FormularioRegistro/>
+
+          <Button type="submit">Enviar</Button>
+        </Form>
+      </ContainForm>
     </ContainerRegister>
   );
-}
+};
 
 export default RegisterAdvisor;
