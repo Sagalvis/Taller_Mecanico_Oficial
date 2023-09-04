3a2acab4029f96a2837ac9d87d0e4dc9cdf72d02
Sergio Galvis <chefosergio@gmail.com>
2023-09-03 19:09:29 -0500

    rediseñando sidebar admin

--- Frontend/src/components/Admin/Layout/styledRuter.jsx
+++ Frontend/src/components/Admin/Layout/styledRuter.jsx
@@ -5,12 +5,17 @@ export const ContainerLayout = styled.div`
     height: 100%;
     width: 100%;
     display: flex;
+    justify-content: flex-start;
+    background-color: ${colors.primary};
 `;
-
+export const Dividier = styled.div`
+    width: 70px;
+`
 export const ContainerPages = styled.div`
     width: 100%;
     height: 100vh;
     display: flex;
     justify-content: center;
-    background-color: ${colors.primary};
+    align-items: center;
+    
 `;
