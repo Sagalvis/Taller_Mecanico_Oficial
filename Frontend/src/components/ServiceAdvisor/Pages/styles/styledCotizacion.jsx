cc77fb4adcf28a77d5e9ea2fada7b90ac88da482
Sergio Galvis <Sgalvis@ulearning.com.co>
2023-09-06 13:06:02 -0500

    se actualizando vista cotizacion

--- Frontend/src/components/ServiceAdvisor/Pages/styles/styledCotizacion.jsx
+++ Frontend/src/components/ServiceAdvisor/Pages/styles/styledCotizacion.jsx
@@ -1,14 +1,51 @@
 import styled from "styled-components";
 
 export const ContainFactura = styled.div`
-    display: flex;
-  /* flex-direction: column; */
+  display: flex;
   justify-content: center;
   align-items: center;
-  background-color: #fff;
-  padding: 1rem;
-  width: 70%;
-  height: 85%;
-  border-radius: 5px;
+  width: 100%;
+  height: 100vh;
+  gap: 10px;
+`;
+
+export const FacturaContain = styled.div`
+  background-color: #ffffff9d;
+  width: 40%;
+  height: 90%;
+  border-radius: 4px;
+  padding: 10px;
   box-shadow: 0 2px 4px rgba(0, 0, 0, 0.301);
+`;
+
+export const ContainerInfoCliente = styled.div`
+  display: flex;
+`
+export const InfoClient = styled.div`
+  display: flex;
+  text-align: center;
+  width: 50%;
+  height: 18%;
+  margin-top: 5px;
+  flex-direction: column;
+  justify-content: space-around;
+  /* background-color: red; */
+`;
+export const ContainInfor = styled.div`
+  display:flex ;
+  align-items: center;
+  width: auto;
+  /* background-color: #08eeee; */
+  height: 20px;
+  gap:10px;
+`
+export const InputDoc = styled.input`
+  border: none;
+  outline: none;
+  background-color: transparent;
+`
+
+export const InforP1 = styled.p`
+  margin: 0;
+  font-weight: bold;
 `
